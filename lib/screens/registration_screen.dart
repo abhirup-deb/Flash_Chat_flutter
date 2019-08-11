d870cc96f3b2a23309d90e29b062d6c303d4a815
abhirup-deb <debabhirup1999@gmail.com>
2019-08-11 17:32:18 +0530

    Spinner Added to Registration

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/components/Rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -12,98 +13,108 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   String email;
   String pass;
+  bool spinner=false;
   final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(color: Colors.grey),
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-               email = value;
-              },
-              decoration: InputDecoration(
-                hintStyle: TextStyle(color: Colors.grey),
-                hintText: "Enter your email",
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+      body: ModalProgressHUD(
+        inAsyncCall: spinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              style: TextStyle(color: Colors.grey),
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                pass = value;
-              },
-              decoration: InputDecoration(
-                hintStyle: TextStyle(color: Colors.grey),
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Colors.grey),
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                 email = value;
+                },
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: "Enter your email",
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.grey),
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  pass = value;
+                },
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Colors.grey),
+                  hintText: 'Enter your password',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
 
-      RoundButton(clr: Colors.blueAccent,txt: 'Register',onPressed: () async {
-            try {
-              final Newuser = await _auth.createUserWithEmailAndPassword(
-                  email: email, password: pass);
-              if(Newuser!=null)
-                Navigator.pushNamed(context, ChatScreen.id);
-            }
-            catch(e){
-              print(e);
-             }
-      },),
-      ],
+        RoundButton(clr: Colors.blueAccent,txt: 'Register',onPressed: () async {
+          setState(() {
+            spinner = true;
+          });
+          try {
+            final Newuser = await _auth.createUserWithEmailAndPassword(
+                email: email, password: pass);
+            if (Newuser != null)
+              Navigator.pushNamed(context, ChatScreen.id);
+            setState(() {
+              spinner = false;
+            });
+          }
+          catch(e){
+                print(e);
+               }
+        },),
+        ],
      ),
     ),
+      ),
    );
   }
 }
